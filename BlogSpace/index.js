05217ef52dd84e5131aa7110979d891387140f83
nathanlao <gla93@sfu.ca>
2023-02-03 14:40:30 -0800

    Slice() the first 5 posts from API and render out

--- BlogSpace/index.js
+++ BlogSpace/index.js
@@ -2,5 +2,17 @@
 fetch("https://jsonplaceholder.typicode.com/posts", {method: "GET"})
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        // Slice the first five posts
+        const postsArray = data.slice(0, 5)
+        console.log(postsArray)
+
+        const postsEl = document.getElementById("posts-list")
+
+        postsArray.forEach(post => {
+            postsEl.innerHTML += `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+            `
+        })
     })
