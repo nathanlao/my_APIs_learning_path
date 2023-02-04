9c6333296121c835b84a71721cf042f4bf95b481
nathanlao <gla93@sfu.ca>
2023-02-03 20:55:31 -0800

    POST data to server

--- BlogSpace/index.js
+++ BlogSpace/index.js
@@ -30,5 +30,16 @@ submitForm.addEventListener("submit", (event) => {
         title: title.value,
         body: body.value
     }
-    console.log(postObj)
+    // console.log(postObj)
+
+    // Send data to server
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify(postObj),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
 })
