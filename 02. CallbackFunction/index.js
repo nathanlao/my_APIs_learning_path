3d714d777a36aee680e94eda1138247627d400d9
nathanlao <gla93@sfu.ca>
2023-02-06 19:55:37 -0800

    Added experiment of using callback function

--- 02. CallbackFunction/index.js
+++ 02. CallbackFunction/index.js
@@ -1,7 +1,80 @@
 const newDeck = document.getElementById("new-deck")
 
-newDeck.addEventListener("click", () => {
+function getNewDeck() {
     fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle")
         .then(res => res.json())
         .then(data => console.log(data))
-}) 
+}
+
+newDeck.addEventListener("click", getNewDeck)
+
+/**
+ * 
+// SetTimeout
+function callBack() {
+    console.log("log something...")
+}
+setTimeout(callBack, 2000)
+
+ */
+
+// array.filter
+const people = [
+    { name: "Jack", hasPet: true, age: 12 },
+    { name: "Jill", hasPet: false, age: 18 },
+    { name: "Alice", hasPet: true, age: 22 },
+    { name: "Bob", hasPet: false, age: 32 },
+]
+
+// Own filter function with a callback function as an argument
+function filterArray(array, callback) {
+    const resultingArray = []
+    for (let i = 0; i < array.length; i++) {
+        const resultItem = callback(array[i])
+        if (resultItem) {
+            resultingArray.push(array[i])
+        }
+    }
+    return resultingArray
+}
+
+function hasPet(people) {
+    return people.hasPet
+}
+
+const peopleHasPets = filterArray(people, hasPet)
+console.log(peopleHasPets)
+
+/**
+ * 
+function hasPet(person) {
+    return person.hasPet
+}
+const peopleHasPet = people.filter(hasPet)
+console.log(peopleHasPet)
+
+// 
+const getOlderPerson = (person) => {
+    return person.age >= 18
+}
+const olderPerson = people.filter(getOlderPerson)
+console.log(olderPerson)
+ * 
+ */
+
+
+/**
+ * 
+Functions are a first-class object in JS
+
+let myNum = 42
+let myNum2 = myNum
+
+// Function expression
+let myFunction = function() {
+    console.log("OK")
+}
+
+let myFunction2 = myFunction
+myFunction2()
+ */
--- AsyncJSNote.md
+++ AsyncJSNote.md
@@ -6,3 +6,6 @@
 - Code that can be run `out of order`. Allows a lengthy operation to start, but finish at a later time `without blocking` other code from running in the meantime
 - JS isn't truly asynchronous, but rather has `callback` mechanisms in place to run commands in a differnent order
 - JS is `single-threaded` means only one command can run at a time
+
+## CallBack Function
+Any function that is `passed as an argument` to another function so that it can be executed in that other function. It allows to `pass behaviour` as a `value`. This is useful in `asynchronous` JS.
