2585b5efc239b7e63eaf5a31df65092ec142f6b6
nathanlao <gla93@sfu.ca>
2023-02-08 15:49:30 -0800

    Fetch 2 cards from API and render to the DOM

--- War/index.js
+++ War/index.js
@@ -1,4 +1,6 @@
 const newDeck = document.getElementById("new-deck")
+const newCard = document.getElementById("new-card")
+const cardEl = document.getElementById("cards")
 
 // Saving deck_id from the returned data
 let deckId
@@ -11,5 +13,21 @@ function getNewDeck() {
             deckId = data.deck_id
         })
 }
+
+function getNewCard() {
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            let html = ""
+            html += `
+                <img src="${data.cards[0].image}" alt="card image">
+                <img src="${data.cards[1].image}" alt="card image">
+            `
+            cardEl.innerHTML = html
+        })
+}
     
 newDeck.addEventListener("click", getNewDeck)
+
+newCard.addEventListener("click", getNewCard)
