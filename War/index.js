25ba7178bbb239966f96d5105f74e54fa4352ff7
nathanlao <gla93@sfu.ca>
2023-02-08 16:51:01 -0800

    Styled the card div slot and render two card separated into the slot

--- War/index.js
+++ War/index.js
@@ -1,6 +1,5 @@
 const newDeck = document.getElementById("new-deck")
 const newCard = document.getElementById("new-card")
-const cardsEl = document.getElementById("cards")
 
 // Saving deck_id from the returned data
 let deckId
@@ -9,7 +8,7 @@ function getNewDeck() {
     fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle")
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            // console.log(data)
             deckId = data.deck_id
         })
 }
@@ -18,14 +17,25 @@ function getNewCard() {
     fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            // console.log(data)
 
-            let html = ""
-            html += `
-                <img src="${data.cards[0].image}" alt="card image" />
-                <img src="${data.cards[1].image}" alt="card image" />
+            // Access the cards element and get its own children HTML collection
+            const cardsContainer = document.getElementById("cards")
+            const firstCardSlot = cardsContainer.children[0]
+            const secondCardSlot = cardsContainer.children[1]
+            
+            //  Render the first card and second card to their own div tag
+            let firstCardhtml = ""
+            firstCardhtml += `
+                <img src="${data.cards[0].image}" alt="card image" class="card"/>
             `
-            cardsEl.innerHTML = html
+            firstCardSlot.innerHTML = firstCardhtml
+
+            let secondCardhtml = ""
+            secondCardhtml += `
+                <img src="${data.cards[1].image}" alt="card image" class="card"/>
+            `
+            secondCardSlot.innerHTML = secondCardhtml
         })
 }
     
